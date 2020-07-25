9bfc32f5f160081b304ef746f258a5c5b18f5f44
Aleksei <alexeyponomarenko92@gmail.com>
2020-07-25 21:09:44 +0300

    Proper font sizes in printouts

--- niceprint.m
+++ niceprint.m
@@ -3,7 +3,7 @@ function niceprint(fig_struct, filename)
  
   printer = sprintf("-d%s", strsplit(filename, '.'){end})
   sizestr = sprintf("-S%d,%d", fig_struct.size(1),  fig_struct.size(2))
-  fontstr = sprintf("-F%s:%d", fig_struct.font,  fig_struct.fontsize)
+  fontstr = sprintf("-F%s", fig_struct.font)
   
   print(fig_struct.f, filename, sizestr, fontstr, printer)
 endfunction
