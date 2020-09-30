0d0c2bf8ba1e3b836b470c079c62da365d06acda
Aleksei <aleksei@noreply.example.org>
2020-09-30 17:02:18 +0300

    Update 'inst/nicetest.m'
    Wrap nicetest as function (prevent scope pollution)

--- inst/nicetest.m
+++ inst/nicetest.m
@@ -1,61 +1,56 @@
-%close all;
-%clear all;
+function nicetest()
+  X = linspace(-pi, pi, 62);
 
-X = linspace(-pi, pi, 62);
+  phases = linspace(eps, 2*pi, 62);
 
-phases = linspace(eps, 2*pi, 62);
+  disp("Testing 2D plot display...");
+  nf = nicefig("fontname", "Latin Modern Roman", "fontsize", 14);
 
-disp("Testing 2D plot display...");
-nf = nicefig("fontname", "Latin Modern Roman", "fontsize", 14);
+  for i = phases
+    nf = niceplot(nf, X, sin(X + i));
+  endfor
 
-for i = phases
-  nf = niceplot(nf, X, sin(X + i));
-endfor
+  xlabel("X values");
+  ylabel("Y values");
+  title("2D figure test");
+  xlim([-pi,pi]);
 
-xlabel("X values");
-ylabel("Y values");
-title("2D figure test");
-xlim([-pi,pi]);
+  disp("Testing 3D waterfall display...");
 
-%niceprint(nf, "sin.png");
+  X = randn(1,1e5);
 
-disp("Testing 3D waterfall display...");
+  X = X + sin(2*pi*[1:length(X)]./length(X));
 
-X = randn(1,1e5);
+  nf2 = nicefig("fontname", "Latin Modern Roman", "fontsize", 14, "cmap", viridis);
 
-X = X + sin(2*pi*[1:length(X)]./length(X));
+  nicewfall(X, 1:length(X), 50, 100, nf2);
 
-nf2 = nicefig("fontname", "Latin Modern Roman", "fontsize", 14, "cmap", viridis);
+  xlabel("X values");
+  ylabel("Y values");
+  zlabel("Hit probability");
+  title("3D waterfall surface test [Gaussian histograms]");
+  view(45,45);
+  xlim([-6,6]);
 
-nicewfall(X, 1:length(X), 50, 100, nf2);
+  disp("Testing 2D plot colormap override, re-capture of old figure...");
 
-xlabel("X values");
-ylabel("Y values");
-zlabel("Hit probability");
-title("3D waterfall surface test [Gaussian histograms]");
-view(45,45);
-xlim([-6,6]);
-%niceprint(nf2, "wfall.png");
+  X = linspace(-pi, pi, 62);
+  cm = copper(length(phases));
 
-disp("Testing 2D plot colormap override, re-capture of old figure...");
+  for i = 1:length(phases)
+    niceplot(nf, X, cos(X + phases(i)), 'Color', cm(ceil(i),:));
+  endfor
 
-X = linspace(-pi, pi, 62);
-cm = copper(length(phases));
+  disp("Testing eye-diagram plotting on noisy sine/cosine wave...");
 
-for i = 1:length(phases)
-  niceplot(nf, X, cos(X + phases(i)), 'Color', cm(ceil(i),:));
-endfor
+  nf3 = nicefig("fontname", "Latin Modern Roman", "fontsize", 14, "cmap", viridis);
 
-disp("Testing eye-diagram plotting on noisy sine/cosine wave...");
+  N = randn(1,5e4)/10;
+  X = [1:length(N)]/100 * 2 * pi;
 
-nf3 = nicefig("fontname", "Latin Modern Roman", "fontsize", 14, "cmap", viridis);
+  niceeye(nf3, [5*sin(X(1:length(X)/2)),5*sin(X((length(X)/2+1):end)+pi)] + N, 100);
+  xlabel("Normalized time");
+  ylabel("Amplitude");
+  title("Eye-diagram, Sine/Cosine");
 
-N = randn(1,5e4)/10;
-X = [1:length(N)]/100 * 2 * pi;
-
-niceeye(nf3, [5*sin(X(1:length(X)/2)),5*sin(X((length(X)/2+1):end)+pi)] + N, 100);
-xlabel("Normalized time");
-ylabel("Amplitude");
-title("Eye-diagram, Sine/Cosine");
-
-%niceprint(nf, "sin.png");
+endfunction
