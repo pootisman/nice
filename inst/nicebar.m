bcdf0ab62e4752205a79bab96f2a1bd6dbba18a9
Aleksei <aleksei@noreply.example.org>
2020-10-11 22:56:12 +0300

    Update 'inst/nicebar.m'
    nicebar now passes varargs to the colorbar function, "southoutside" is removed from defaults

--- inst/nicebar.m
+++ inst/nicebar.m
@@ -1,18 +1,24 @@
-function cb = nicebar(varargin)
-  fontname = "Arial";
-  fontsize = 14;
+function cb = nicebar(nf, varargin)
+  fontname = nf.fontname;
+  fontsize = nf.fontsize;
+
+  reduce_vararg_len = 1;
 
   for i = 1:length(varargin)
     if strcmp(varargin{i}, "fontname")
       fontname = varargin{i+1};
       i=i+1;
+      reduce_vararg_len = reduce_vararg_len + 2;
     elseif strcmp(varargin{i}, "fontsize")
       fontsize = varargin{i+1};
       i=i+1;
+      reduce_vararg_len = reduce_vararg_len + 2;
     end
   end
 
-  cb = colorbar("southoutside");
+  varargin = varargin(reduce_vararg_len:end)
+
+  cb = colorbar(varargin{:});
   set(cb, 'fontname', fontname);
   set(cb, 'fontsize', fontsize);
 end
