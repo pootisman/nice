e5c078e0aa533cda38c4a9978463c5567d888f92
Aleksei <alexeyponomarenko92@gmail.com>
2021-02-14 17:55:04 +0200

    Adding documentation/License info

--- inst/niceeye.m
+++ inst/niceeye.m
@@ -1,3 +1,34 @@
+## Copyright (C) 2020-2021 Aleksei Ponomarenko-Timofeev
+## 
+## This file is part of Nice plotter package.
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## usage: fs = niceeye(nf, sig, sps, varargin)
+##
+## Draw eye-diagram on instance of Nice Figure nf
+##
+## nf - root struct for Nice Figure to draw on
+## sig - Overall signal to plot
+## sps - Samples per symbol
+## varargin may contain following parameters:
+## "Color" - use builtin figure colormap
+##
+## output "fs" is a root struct for Nice Figure, should be passed
+## to other nice functions to draw modify content in figure
+##
+
 function nf = niceeye(nf, sig, sps, varargin)
   assert(isstruct(nf), "niceplot expects first argument to be a nicefig struct");
   
--- inst/nicehist.m
+++ inst/nicehist.m
@@ -1,4 +1,34 @@
-function nf = nicehist(x, y, varargin)
+## Copyright (C) 2020-2021 Aleksei Ponomarenko-Timofeev
+## 
+## This file is part of Nice plotter package.
+##
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+##
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+## usage: fs = nicehist(nf, y, varargin)
+##
+## Draw histogram on instance of Nice Figure nf
+##
+## nf - root struct for Nice Figure to draw on 
+## x, y - bin centers and weights
+## varargin may contain following parameters:
+## "Color" - use built-in figure colormap
+##
+## output "fs" is a root struct for Nice Figure, should be passed
+## to other nice functions to draw modify content in figure
+##
+
+function nf = nicehist(nf, x, y, varargin)
   assert(isstruct(nf), "niceplot expects first argument to be a nicefig struct");
   
   figure(nf.f);
